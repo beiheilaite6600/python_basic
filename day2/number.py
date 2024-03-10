9e229b32ecf322c96cbf3bb8a2e3c46edc72f13c
i310283 <ling.zuo@sap.com>
2024-03-11 00:06:37 +0800

    define mutiple variables in one line

--- day2/number.py
+++ day2/number.py
@@ -1,11 +1,9 @@
 # Three types of Number: integer, float, imaginary number.
-
 my_integer = 100 # Int
 my_float = 100.12 # float
-my_imaginary_number = complex(100, 1)
+my_imaginary_number = complex(100, 1) # imaginary number
 
 # + - * /
-
 a = my_integer + my_float
 b = my_imaginary_number / my_float
 c = my_integer * my_imaginary_number
@@ -13,3 +11,8 @@ print(a)
 print(b)
 print(c)
 
+
+# define mutiple variables in one line. 
+my_integer, my_float, my_imaginary_number = 1, 1.1, complex(1, 1)
+
+print(my_integer, my_float, my_imaginary_number)
