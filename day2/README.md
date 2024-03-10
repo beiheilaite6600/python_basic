f78cbdbc39b0c4844b51537f0f742512af396292
i310283 <ling.zuo@sap.com>
2024-03-10 23:22:30 +0800

    day2 README and comment.py
    Co-authored-by: beiheilaite6600 <beiheilaite6600@users.noreply.github.com>

---
+++ day2/README.md
@@ -0,0 +1,6 @@
+## Day2
+
+1. Python variable definition: No Needed to identify the type. e.g,
+   1. my_string = "123" # string type, variable name is my_string. 
+   2. my_number = 123 # number type, variable name is my_number. 
+2. Python 
---
+++ day2/comment.py
@@ -0,0 +1,13 @@
+# signle line comment
+my_string = "123" # String type, default value is "123"
+my_number = 123 # Nubmer type, default valude is int 123. 
+
+# hahahaha
+# my_string='123'
+# my_number=123
+my_string_add=my_string +5
+my_number_add=my_number +2
+
+print(my_string_add)
+print(my_number_add)
+
