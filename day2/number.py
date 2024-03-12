1488bc0c4e7fc7279af69e828ec63f8e6119fd1c
i310283 <ling.zuo@sap.com>
2024-03-12 23:31:31 +0800

    initialize day3 Co-authored-by: beiheilaite6600 <beiheilaite6600@users.noreply.github.com>

--- day2/number.py
+++ day2/number.py
@@ -1,18 +1,18 @@
 # Three types of Number: integer, float, complex.
 my_integer = 100 # Int
 my_float = 100.12 # float
-my_imaginary_number = complex(100, 1) # complex
+my_complex_number = complex(100, 1) # complex
 
 # + - * /
 a = my_integer + my_float
-b = my_imaginary_number / my_float
-c = my_integer * my_imaginary_number
+b = my_complex_number / my_float
+c = my_integer * my_complex_number
 print(a)
 print(b)
 print(c)
 
 
-# define mutiple variables in one line. 
-my_integer, my_float, my_imaginary_number = 1, 1.1, complex(1, 1)
+# declare mutiple variables in one line. 
+my_integer, my_float, my_complex_number = 1, 1.1, complex(1, 1)
 
-print(my_integer, my_float, my_imaginary_number)
+print(my_integer, my_float, my_complex_number)
